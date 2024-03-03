e2e60a5902817d03825168565b3cb7674ba661ce
Ansh Upadhyay <upadhyayaansh094@gmail.com>
2024-03-03 14:41:25 +0530

    Commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const H2 = React.createElement('h2',{
     style:{
         color:'rgb(27,27,27)',
         fontSize:'50px',
+        textAlign:'start'
+
     },
 },'Learn web development');
 
@@ -11,7 +13,11 @@ const H2 = React.createElement('h2',{
 const Para1 = React.createElement('p',{
     style:{
         marginBottom:'20px',
-        textAlign:'center',
+        textAlign:'start',
+        fontSize:'20px',
+        lineHeight:'1.6rem'
+
+    
 
     }
 },'Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.');
@@ -19,7 +25,10 @@ const Para1 = React.createElement('p',{
 const Para2 = React.createElement('p',{
     style:{
         marginBottom:'20px',
-        textAlign:'center',
+        textAlign:'start',
+        fontSize:'20px',
+        lineHeight:'1.6rem'
+
 
     }
 },'The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from ', React.createElement('a', { href: 'https://developer.mozilla.org/' }, 'the rest of MDN'), ', and other intermediate to advanced resources that assume a lot of previous knowledge.');
@@ -27,15 +36,20 @@ const Para2 = React.createElement('p',{
 const Para3 = React.createElement('p',{
     style:{
         marginBottom:'20px',
-        textAlign:'center',
+    textAlign:'start',
+    fontSize:'20px',
+    lineHeight:'1.6rem'
+
         
     }
 },'If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.');
 
 const mainDivStyle = {
-width:'84%',
-    margin:'3%',
-    padding:'5%'
+width:'51%',
+    margin:'10%',
+    marginTop:'0%',
+    padding:'5%',
+    paddingTop:'0%',
 
 }
 const div1 = React.createElement('div',{},[Para1]);
